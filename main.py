f1c3b104190ce011853c9db2ce5799dab3363b51
rahul-8437008949 <54345729+rahul-8437008949@users.noreply.github.com>
2019-11-04 15:54:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, flash, url_for, redirect, sessions, session
+from flask import Flask, render_template, request, flash, url_for, redirect, session
 import pyodbc
 import random
 
